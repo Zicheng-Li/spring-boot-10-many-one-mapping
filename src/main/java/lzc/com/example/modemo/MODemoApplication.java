96adfd9fc9048dc09e4887da9d357761be29d980
ZL&EL <lzc123456ab@gmail.com>
2023-07-01 17:56:16 -0400

    4

--- src/main/java/lzc/com/example/modemo/MODemoApplication.java
+++ src/main/java/lzc/com/example/modemo/MODemoApplication.java
@@ -1,7 +1,13 @@
 package lzc.com.example.modemo;
 
+import lzc.com.example.modemo.dao.AppDAO;
+import lzc.com.example.modemo.entity.Course;
+import lzc.com.example.modemo.entity.Instructor;
+import lzc.com.example.modemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MODemoApplication {
@@ -10,4 +16,42 @@ public class MODemoApplication {
 		SpringApplication.run(MODemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+		return runner -> {
+//			 createInstructor(appDAO);
+			// findInstructor(appDAO);
+			//deleteInstructor(appDAO);
+//			findInstructorDetail(appDAO);
+//			deleteInstructorDetail(appDAO);
+			createInstructorWithCourse(appDAO);
+
+		};
+	}
+
+	private void createInstructorWithCourse(AppDAO appDAO) {
+// create instructor detail
+		Instructor tempInstructor = new Instructor("HHHH", "lplplplp", "hhhh@rbc.com");
+
+		InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.youtube.com", "code");
+
+		// associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		// create the courses
+		Course tempCourse1 = new Course("C++");
+		Course tempCourse2 = new Course("Java");
+
+		// add courses to instructor
+		tempInstructor.add(tempCourse1);
+		tempInstructor.add(tempCourse2);
+
+		// save the instructor
+		// note: this will ALSO save the course because of CascadeType.ALL
+		System.out.println("Saving instructor: " + tempInstructor);
+		System.out.println("Saving the course " + tempInstructor.getCourses() );
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done!");
+	}
 }
