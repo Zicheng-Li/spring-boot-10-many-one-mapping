be8327e880a47aa5c79f5427f85f1b66e420c2c1
ZL&EL <lzc123456ab@gmail.com>
2023-07-01 17:23:58 -0400

    1

---
+++ src/main/java/lzc/com/example/modemo/MODemoApplication.java
@@ -0,0 +1,13 @@
+package lzc.com.example.modemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MODemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MODemoApplication.class, args);
+	}
+
+}
